f33b41ca2d2b046fb534b6e88cbb9b1cf5b78bff
cvrcoe26 <guru02721@gmail.com>
2025-04-22 21:09:17 +0530

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -1,5 +1,5 @@
 // first create a folder
-// command prompt navigate to that folder
+// open command prompt (terminal) and navigate to that folder
 // run the following commands
 // npm init -y
 // npm install express axios
@@ -34,17 +34,20 @@ app.post('/weather', async (req, res) => {
 });
 app.listen(3000, () => console.log('Server running'));
 
-// create a folder named views and create the following files in it
+// create a folder with name views and create the following files in that folder:
 
-//views/home.ejs
+// home.ejs
 <form action="/weather" method="POST">
   <input type="text" name="city" placeholder="Enter city">
   <button type="submit">Get Weather</button>
 </form>
 
 
-//views/weather.ejs
+// weather.ejs
 <h2>Weather in <%= city %></h2>
 <p>Temperature: <%= temp %>°C</p>
 <p>Humidity: <%= humidity %>%</p>
 <p>Condition: <%= condition %></p>
+
+
+/// access the app at: http://localhost:3000
